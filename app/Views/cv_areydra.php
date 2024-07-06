5dfcc0a9d0b83032700d3aa6d0feb5a9d749da1d
Areydra Desfikriandre <areydra@gmail.com>
2024-07-06 12:00:16 +0700

    Create a new Curriculum Vitae

---
+++ app/Views/cv_areydra.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CV - Areydra Desfikriandre</title>
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/styles.css'); ?>">
+</head>
+<body>
+  <div class="grid-container">
+    <div class="sidebar">
+      <div>
+        <img
+          class="image-photos"
+          src="<?php echo base_url('assets/images/photos/areydra.jpg'); ?>"
+          alt="phone-icon"
+        >
+        <p class="contact-text">
+          <img
+            class="image-icon"
+            src="<?php echo base_url('assets/images/icons/phone.png'); ?>"
+            alt="phone-icon"
+          >
+          +6283807106451
+        </p>
+
+        <p class="contact-text">
+          <img
+            class="image-icon"
+            src="<?php echo base_url('assets/images/icons/mail.png'); ?>"
+            alt="mail-icon"
+          >
+          areydra@gmail.com
+        </p>
+        <p class="contact-text">
+          <img
+            class="image-icon"
+            src="<?php echo base_url('assets/images/icons/map.png'); ?>"
+            alt="map-icon"
+          >
+          Bogor, West Java, Indonesia
+        </p>
+      </div>
+      <div>
+        <h1 class="education-title-text">
+          EDUCATION
+        </h1>
+        <p class="education-graduate-text">
+          Universitas Mercu Buana
+        </p>
+        <p class="education-vocational-text">
+          Teknik Informatika
+        </p>
+        <p class="education-years-text">
+          2022 - Present
+        </p>
+
+        <p class="education-graduate-text">
+          Arkademy Tech Education
+        </p>
+        <p class="education-vocational-text">
+          React Developer
+        </p>
+        <p class="education-years-text">
+          2019 - 2019
+        </p>
+
+        <p class="education-graduate-text">
+          SMK Al Bahri
+        </p>
+        <p class="education-vocational-text">
+          Rekayasa Perangkat Lunak
+        </p>
+        <p class="education-years-text">
+          2016 - 2019
+        </p>
+      </div>
+
+      <div>
+        <h1 class="expertise-title-text">EXPERTISE</h1>
+        <p class="expertise-list-text">React Native</p>
+        <p class="expertise-list-text">Javascript</p>
+        <p class="expertise-list-text">Typescript</p>
+        <p class="expertise-list-text">React.js</p>
+      </div>
+    </div>
+    <div class="main">
+      <p class="name-text">
+        AREYDRA DESFIKRIANDRE
+      </p>
+      <p class="role-text">
+        Software Engineer
+      </p>
+      <div class="separator"></div>
+
+      <h1 class="about-me-title-text">ABOUT ME</h1>
+      <p class="about-me-text">
+        Experienced professional skilled in research, documentation, and collaborative teamwork. Proficient in analyzing new product features, technical issues, and workflow enhancements, especially in mobile engineering. Adept at collaborating with internal and external teams to achieve project goals. Committed to continuous improvement, open to feedback, and proactive in seeking and providing assistance. Passionate about learning by doing and contributing to innovative solutions.
+      </p>
+
+      <h1 class="work-experience-title-text">
+        WORK EXPERIENCE
+      </h1>
+      <div class="work-experience-container-role">
+        <p class="work-experience-years-location-title-text">
+          Feb 2022 - Present
+        </p>
+        <p class="work-experience-years-location-title-text">
+          Flip | Jakarta
+        </p>
+        <p class="work-experience-se-text">
+          Software Engineer
+        </p>
+        <ul class="list-container">
+          <li>
+            <p class="work-experience-list-text">
+              I conducted extensive research, creating detailed documents on new product features and technical issues, enhancing performance and best practices.
+            </p>
+          </li>
+          <li>
+            <p class="work-experience-list-text">
+              I optimized workflow processes in mobile engineering, fostering efficiency. Collaborating effectively with internal and external teams, I facilitated the achievement of project objectives.
+            </p>
+          </li>
+          <li>
+            <p class="work-experience-list-text">
+              I developed applications with minimum defect, focusing on performance and code readability, and implemented rigorous unit and UI testing.
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div class="work-experience-container-role">
+        <p class="work-experience-years-location-title-text">
+          Dec 2019 - Apr 2022
+        </p>
+        <p class="work-experience-years-location-title-text">
+          Flip | Jakarta
+        </p>
+        <p class="work-experience-md-text">
+          Mobile Developer
+        </p>
+        <ul class="list-container">
+          <li>
+            <p class="work-experience-list-text">
+              As a junior developer I'm developing an application that focuses on achieving minimum defect results and collaborating with colleagues by reviewing their code.
+            </p>
+          </li>
+          <li>
+            <p class="work-experience-list-text">
+              As a junior developer I also contributed to the upload process on the Play Store and App Store.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+</div>
+</body>
+</html>
